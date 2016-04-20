6dd3d1989b247996cfd6c403019ae95e64280564
Warren Krewenki <krewenki@gmail.com>
2016-04-20 11:35:24 -0300

    Move CRUD to prototype model, move connection to model and make it resolve a promise.

--- src/crud.js
+++ src/crud.js
@@ -1,30 +1,40 @@
-var DB = {
+var Promise = require('promise');
+var DB = function(uri){
 
-  fetchAllRecords: function(collection, search, callback){
-		callback = typeof callback == 'function' ? callback : function(err, docs){};
-		search = search || {};
-		self.db.collection(collection).find(search, callback);
-	},
+  function DB(uri){
+    MongoClient.connect('mongodb://'+sanitizeURI(uri), function(){
+      //listCollectionsCallback
+    });
+  }
 
-	createRecord function(record, collection, callback){
+  function sanitizeURI(uri){
+    return uri.replace('mongodb://','');
+  }
+
+}
+
+DB.prototype.fetchAllRecords =function(collection, search, callback){
+	callback = typeof callback == 'function' ? callback : function(err, docs){};
+	search = search || {};
+	self.db.collection(collection).find(search, callback);
+};
+
+DB.prototype.createRecord = function(record, collection, callback){
 		callback = typeof callback == 'function' ? callback : function(err, docs){};
 		self.db.collection(collection).insert(record, callback);
-	},
+};
 
-	updateRecord: function(record, collection, callback){
+DB.prototype.updateRecord = function(record, collection, callback){
 		var tosave = _.clone(record);
 		delete tosave._id;
 		callback = typeof callback == 'function' ? callback : function(err, docs){};
 		self.db.collection(collection).update({ "_id": ObjectId(record._id) }, { $set: tosave }
 		, callback)
-	},
+};
 
-	deleteRecord, function(_id, collection, callback){
+DB.prototype.deleteRecord = function(_id, collection, callback){
 		callback = typeof callback == 'function' ? callback : function(err, docs){};
 		self.db.collection(collection).remove({ "_id" : ObjectId(_id)}, callback );
-	}
-
-}
-
+};
 
 module.exports = DB;
