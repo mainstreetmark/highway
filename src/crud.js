d001295eed8559d9f186784ecc1493b6860e8df7
Warren Krewenki <krewenki@gmail.com>
2016-04-21 11:50:25 -0300

    More hook work and some documentation.

--- src/crud.js
+++ src/crud.js
@@ -15,9 +15,15 @@ var DB = function ( uri, options ) {
 
 }
 
-DB.prototype.connect = function ( fulfill, reject ) {
+/**
+ * Connect to a mongo database and return a promise
+ * @method connect
+ * @param  {string} uri An optional uri to connect to.
+ * @return {instance} An instance of MongoClient
+ */
+DB.prototype.connect = function ( uri ) {
 	return new Promise( function ( fulfill, reject ) {
-			MongoClient.connect( 'mongodb://' + this.uri ),
+			MongoClient.connect( 'mongodb://' + ( uri || this.uri ) ),
 				function ( err, db ) {
 					if ( err ) reject( err );
 					else fulfill( db );
@@ -25,41 +31,112 @@ DB.prototype.connect = function ( fulfill, reject ) {
 	} );
 };
 
+
+/**
+ * Fetch all records from a specific collection, using specified search parameters
+ * and return a promise
+ *
+ * @method fetchAllRecords
+ * @param  {string}        collection The name of the collection you want to fetch
+ * @param  {object}        search     Parameters to apply to your search
+ * @param  {Function}      callback   A method to execute when the search completes
+ * @return {promise}        A promise
+ */
 DB.prototype.fetchAllRecords = function ( collection, search, callback ) {
 	callback = typeof callback == 'function' ? callback : function ( err, docs ) {};
 	search = search || {};
-	self.db.collection( collection )
-		.find( search, callback );
+	return new Promise( function ( success, failure ) {
+		self.db.collection( collection )
+			.find( search, function ( err, docs ) {
+				if ( err ) failure( err );
+				else {
+					callback( err, docs );
+					success( docs );
+				}
+			} );
+	} )
+
 };
 
+/**
+ * [createRecord description]
+ * @method createRecord
+ * @param  {[type]}     record     [description]
+ * @param  {[type]}     collection [description]
+ * @param  {Function}   callback   [description]
+ * @return {[type]}     [description]
+ */
 DB.prototype.createRecord = function ( record, collection, callback ) {
-	record = this.hook( collection, 'beforeSave', record );
-	callback = typeof callback == 'function' ? callback : function ( err, docs ) {};
-	self.db.collection( collection )
-		.insert( record, callback );
+	return this.hook( collection, 'beforeSave', record )
+		.then( function ( record ) {
+			callback = typeof callback == 'function' ? callback : function ( err, docs ) {};
+			self.db.collection( collection )
+				.insert( record, callback );
+		} )
 };
 
+/**
+ * [updateRecord description]
+ * @method updateRecord
+ * @param  {[type]}     record     [description]
+ * @param  {[type]}     collection [description]
+ * @param  {Function}   callback   [description]
+ * @return {[type]}     [description]
+ */
 DB.prototype.updateRecord = function ( record, collection, callback ) {
-	record = this.hook( collection, 'beforeSave', record );
-	var tosave = _.clone( record );
-	delete tosave._id;
-	callback = typeof callback == 'function' ? callback : function ( err, docs ) {};
-	self.db.collection( collection )
-		.update( {
-			"_id": ObjectId( record._id )
-		}, {
-			$set: tosave
-		}, callback )
+	return this.hook( collection, 'beforeSave', record )
+		.then( function ( record ) {
+			var tosave = _.clone( record );
+			delete tosave._id;
+			callback = typeof callback == 'function' ? callback : function ( err, docs ) {};
+			return new Promise( function ( success, error ) {
+				self.db.collection( collection )
+					.update( {
+						"_id": ObjectId( record._id )
+					}, {
+						$set: tosave
+					}, function ( err, docs ) {
+						if ( err )
+							error( err );
+						else {
+							success( docs );
+						}
+					} )
+			} );
+		} )
 };
 
+/**
+ * [deleteRecord description]
+ * @method deleteRecord
+ * @param  {[type]}     _id        [description]
+ * @param  {[type]}     collection [description]
+ * @param  {Function}   callback   [description]
+ * @return {[type]}     [description]
+ */
 DB.prototype.deleteRecord = function ( _id, collection, callback ) {
 	callback = typeof callback == 'function' ? callback : function ( err, docs ) {};
-	self.db.collection( collection )
-		.remove( {
-			"_id": ObjectId( _id )
-		}, callback );
+	return new Promise( function ( success, failure ) {
+		self.db.collection( collection )
+			.remove( {
+				"_id": ObjectId( _id )
+			}, function ( err, doc ) {
+				if ( err ) failure( err );
+				callback();
+				success( doc );
+			} );
+	} )
+
 };
 
+/**
+ * [hook description]
+ * @method hook
+ * @param  {[type]} collection [description]
+ * @param  {[type]} hook       [description]
+ * @param  {[type]} data       [description]
+ * @return {[type]} [description]
+ */
 DB.prototype.hook = function ( collection, hook, data ) {
 	return new Promise( function ( success, failure ) {
 		if ( this.options.hooks && this.options.hooks[ collection ] && typeof this.options.hooks[ collection ][ hook ] == 'function' )
@@ -68,7 +145,6 @@ DB.prototype.hook = function ( collection, hook, data ) {
 			} catch ( x ) {
 				failure( x );
 			}
-
 	} );
 
 
