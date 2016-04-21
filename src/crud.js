635d0ebd44fefa1342aafc6b289fc32eb6c1aa2f
Warren Krewenki <krewenki@gmail.com>
2016-04-21 10:48:44 -0300

    More work on hooks.  They are trickier than I thought when everything is async

--- src/crud.js
+++ src/crud.js
@@ -1,65 +1,77 @@
-var Promise = require('promise');
-var DB = function(uri, options) {
+var Promise = require( 'promise' );
+var DB = function ( uri, options ) {
 
-    if (!uri) {
-        console.log('No URI provided. No database connection available');
-    }
-    this.uri = sanitizeURI(uri);
-    this.options = options || {};
+	if ( !uri ) {
+		console.log( 'No URI provided. No database connection available' );
+	}
+	this.uri = sanitizeURI( uri );
+	this.options = options || {};
 
-    function sanitizeURI(uri) {
-        return uri.replace('mongodb://', '');
-    }
+	function sanitizeURI( uri ) {
+		return uri.replace( 'mongodb://', '' );
+	}
 
-    return this;
+	return this;
 
 }
 
-DB.prototype.connect = function(fulfill, reject) {
-    return new Promise(function(fulfill, reject) {
-            MongoClient.connect('mongodb://' + this.uri),
-                function(err, db) {
-                    if (err) reject(err);
-                    else fulfill(db);
-                });
-    });
+DB.prototype.connect = function ( fulfill, reject ) {
+	return new Promise( function ( fulfill, reject ) {
+			MongoClient.connect( 'mongodb://' + this.uri ),
+				function ( err, db ) {
+					if ( err ) reject( err );
+					else fulfill( db );
+				} );
+	} );
 };
 
-DB.prototype.fetchAllRecords = function(collection, search, callback) {
-    callback = typeof callback == 'function' ? callback : function(err, docs) {};
-    search = search || {};
-    self.db.collection(collection).find(search, callback);
+DB.prototype.fetchAllRecords = function ( collection, search, callback ) {
+	callback = typeof callback == 'function' ? callback : function ( err, docs ) {};
+	search = search || {};
+	self.db.collection( collection )
+		.find( search, callback );
 };
 
-DB.prototype.createRecord = function(record, collection, callback) {
-    record = this.hook(collection, 'beforeSave', record);
-    callback = typeof callback == 'function' ? callback : function(err, docs) {};
-    self.db.collection(collection).insert(record, callback);
+DB.prototype.createRecord = function ( record, collection, callback ) {
+	record = this.hook( collection, 'beforeSave', record );
+	callback = typeof callback == 'function' ? callback : function ( err, docs ) {};
+	self.db.collection( collection )
+		.insert( record, callback );
 };
 
-DB.prototype.updateRecord = function(record, collection, callback) {
-    record = this.hook(collection, 'beforeSave', record);
-    var tosave = _.clone(record);
-    delete tosave._id;
-    callback = typeof callback == 'function' ? callback : function(err, docs) {};
-    self.db.collection(collection).update({
-        "_id": ObjectId(record._id)
-    }, {
-        $set: tosave
-    }, callback)
+DB.prototype.updateRecord = function ( record, collection, callback ) {
+	record = this.hook( collection, 'beforeSave', record );
+	var tosave = _.clone( record );
+	delete tosave._id;
+	callback = typeof callback == 'function' ? callback : function ( err, docs ) {};
+	self.db.collection( collection )
+		.update( {
+			"_id": ObjectId( record._id )
+		}, {
+			$set: tosave
+		}, callback )
 };
 
-DB.prototype.deleteRecord = function(_id, collection, callback) {
-    callback = typeof callback == 'function' ? callback : function(err, docs) {};
-    self.db.collection(collection).remove({
-        "_id": ObjectId(_id)
-    }, callback);
+DB.prototype.deleteRecord = function ( _id, collection, callback ) {
+	callback = typeof callback == 'function' ? callback : function ( err, docs ) {};
+	self.db.collection( collection )
+		.remove( {
+			"_id": ObjectId( _id )
+		}, callback );
 };
 
-DB.prototype.hook = function(collection, hook, data) {
-    if (this.options.hooks && this.options.hooks[collection] && typeof this.options.hooks[collection][hook] == 'function')
-        data = this.options.hooks[collection][hook](data);
-    return data;
+DB.prototype.hook = function ( collection, hook, data ) {
+	return new Promise( function ( success, failure ) {
+		if ( this.options.hooks && this.options.hooks[ collection ] && typeof this.options.hooks[ collection ][ hook ] == 'function' )
+			try ( data = this.options.hooks[ collection ][ hook ]( data ); ) {
+				success( data );
+			} catch ( x ) {
+				failure( x );
+			}
+
+	} );
+
+
 }
 
 module.exports = DB;
