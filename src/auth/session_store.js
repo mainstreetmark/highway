b3a2bc8a894fb1ed357c54013aece0ff72fd7a0a
Warren Krewenki <krewenki@gmail.com>
2016-05-03 16:14:40 -0300

    Use connect-mongodb-session for sessions

--- src/auth/session_store.js
+++ src/auth/session_store.js
@@ -1,18 +1,20 @@
-function SessionStore( session, db ) {
+function SessionStore( session, uri ) {
 	var self = this;
 	return new Promise( function ( success, failure ) {
-		var MongoStore = require( 'connect-mongostore' )( session )
-		try {
-			self.store = new MongoStore( {
-				db: db
-			}, function ( a, b, c ) {
-				console.log( 'session store created' );
-				success( self );
-			} )
-		} catch ( x ) {
-			failure( x );
-		}
-	} )
+		var MongoStore = require( 'connect-mongodb-session' )( session )
+		self.store = new MongoStore( {
+			uri: uri,
+			collection: 'sessions'
+		} )
+
+		self.store.on( 'connected', function () {
+			success( self.stroe );
+		} )
+
+		self.store.on( 'error', function ( error ) {
+			failure( error );
+		} )
+	} );
 }
 
 module.exports = SessionStore;
