2497ab4950dcf6e21f9bb23f2732b66efa13c1b3
Warren Krewenki <krewenki@gmail.com>
2016-05-09 06:20:45 -0300

    Use connect-mongodb-store instead of connect-mongostore

--- src/auth/session_store.js
+++ src/auth/session_store.js
@@ -1,19 +1,24 @@
 function SessionStore( session, uri ) {
 	var self = this;
 	return new Promise( function ( success, failure ) {
-		var MongoStore = require( 'connect-mongodb-session' )( session )
+		var MongoStore = require( 'connect-mongodb-session' )( session );
+
+		if ( uri.indexOf( 'mongodb://' ) <= -1 ) {
+			uri = 'mongodb://' + uri;
+		}
+
 		self.store = new MongoStore( {
 			uri: uri,
 			collection: 'sessions'
-		} )
+		} );
 
 		self.store.on( 'connected', function () {
-			success( self.stroe );
-		} )
+			success( self.store );
+		} );
 
 		self.store.on( 'error', function ( error ) {
 			failure( error );
-		} )
+		} );
 	} );
 }
 
