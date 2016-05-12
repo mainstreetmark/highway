2d26392e3dff15d06acc1efe47397bd4f567a87b
Warren Krewenki <krewenki@gmail.com>
2016-05-12 05:44:27 -0300

    Add expect.js to package.json, vs expect.

--- test/syntax.js
+++ test/syntax.js
@@ -1,36 +1,40 @@
-var expect = require('expect.js');
+var expect = require( 'expect.js' );
 
-describe('syntax', function(){
-  it('highway should be valid',function(){
-	  var Highway = require( '../highway.js' );
-	  expect(typeof Highway).to.equal('function');
-  })
+describe( 'syntax', function () {
+	it( 'highway should be valid', function () {
+		var Highway = require( '../highway.js' );
+		expect( typeof Highway )
+			.to.equal( 'function' );
+	} );
 
-  it('crud should be valid',function(){
-	  var DB = require( '../src/crud.js' );
-	  expect(typeof DB).to.equal('function');
-  })
-  
-  it('email should be valid',function(){
-	  var Email = require( '../src/email.js' );
-	  expect(typeof Email).to.equal('function');
-  })
+	it( 'crud should be valid', function () {
+		var DB = require( '../src/crud.js' );
+		expect( typeof DB )
+			.to.equal( 'function' );
+	} );
 
-});
+	it( 'email should be valid', function () {
+		var Email = require( '../src/email.js' );
+		expect( typeof Email )
+			.to.equal( 'function' );
+	} );
 
-var Highway = require('../highway.js');
+} );
 
-describe('Instantiating', function(){
-	
-	it('should fail when no config is passed', function(){
+var Highway = require( '../highway.js' );
+
+describe( 'Instantiating', function () {
+
+	it( 'should fail when no config is passed', function () {
 		var err = true;
 		try {
 			var hw = new Highway();
 			err = false;
-		} catch(x) {
-			
-		}
-		expect(err).to.equal(true);
+		} catch ( x ) {
 
-	})
-})
+		}
+		expect( err )
+			.to.equal( true );
+
+	} );
+} );
