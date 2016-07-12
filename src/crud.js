6f693ef7761dc892fe011d56fdba141149e88cf4
Warren Krewenki <krewenki@gmail.com>
2016-07-12 07:03:29 -0300

    Remove it already, because telegauge dislikes it.

--- src/crud.js
+++ src/crud.js
@@ -52,7 +52,7 @@ DB.prototype.connect = function (uri) {
 					return m.trim()
 						.toString();
 				});
-				self.log('info', address + "\tConnected to database");
+				self.log('info', "\tConnected to database");
 				fulfill(self);
 			}
 		});
