efd9f6c3fd18195fd6fc2acaddff93965b577a3a
Mark Young <mainstreetmark@gmail.com>
2018-02-15 10:47:35 -0500

    misplaced )

--- highway.js
+++ highway.js
@@ -1,38 +1,38 @@
-var Highway = function (settings) {
-	var ObjectId = require('mongojs')
+const Highway = function (settings) {
+	const ObjectId = require('mongojs')
 		.ObjectId;
-	var _ = require('underscore');
-	var express = require('express');
-	var Email = require('./src/email.js');
-	var DB = require('./src/crud.js');
-	var reststop = require('./src/rest-stop.js');
-	var SocketServer = require('./src/socket.js');
-	var Auth = require('./src/auth.js');
-	var winston = require('winston');
+	const _ = require('underscore');
+	const express = require('express');
+	const Email = require('./src/email.js');
+	const DB = require('./src/crud.js');
+	const reststop = require('./src/rest-stop.js');
+	const SocketServer = require('./src/socket.js');
+	const Auth = require('./src/auth.js');
+	const winston = require('winston');
 
-	var defaults = {
-		io: false,
-		http: false,
-		uri: false,
+	const defaults = {
+		io      : false,
+		http    : false,
+		uri     : false,
 		database: false,
-		auth: [],
-		email: {},
-		log: false
+		auth    : [],
+		email   : {},
+		log     : false,
 	};
 
-	if (!settings) {
+	if (!settings)
 		throw new Error('No settings provided');
-	}
+
 
 	// logger crap
 	if (settings && settings.log) {
 		winston.add(winston.transports.File, {
 			filename: '/var/log/highway.log',
-			json: false
+			json    : false,
 		});
 		winston.remove(winston.transports.Console);
 	}
-	var self = this;
+	const self = this;
 	self.settings = _.defaults(settings || {}, defaults);
 	self.io = self.settings.io;
 	self.socketservers = self.sockets = {};
@@ -40,27 +40,26 @@ var Highway = function (settings) {
 	self.logger = self.settings.log ? winston.log : function () {};
 
 
-
 	// get the IP address of the device this is running on
-	var os = require('os');
-	var ifaces = os.networkInterfaces();
+	const os = require('os');
+	const ifaces = os.networkInterfaces();
 
-	Object.keys(ifaces).forEach(function (ifname) {
-		var alias = 0;
+	Object.keys(ifaces).forEach((ifname) => {
+		let alias = 0;
 
-		ifaces[ifname].forEach(function (iface) {
-			if ('IPv4' !== iface.family || iface.internal !== false) {
+		ifaces[ifname].forEach((iface) => {
+			if (iface.family !== 'IPv4' || iface.internal !== false)
 				// skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
 				return;
-			}
 
-			if (alias >= 1) {
+
+			if (alias >= 1)
 				// this single interface has multiple ipv4 addresses
 				self.ip = iface.address;
-			} else {
+			 else
 				// this interface has only one ipv4 adress
 				self.ip = iface.address;
-			}
+
 			++alias;
 		});
 	});
@@ -70,63 +69,62 @@ var Highway = function (settings) {
 		if (!self.settings.http)
 			return;
 
-		var bodyParser = require('body-parser');
-		var cookieParser = require('cookie-parser');
+		const bodyParser = require('body-parser');
+		const cookieParser = require('cookie-parser');
 
 		self.settings.http.use(cookieParser());
 		self.settings.http.use(bodyParser.urlencoded({
-			extended: true
+			extended: true,
 		}));
 		self.settings.http.use(bodyParser.json());
 		return true;
 	}
 
 
-	return new Promise(function (success, failure) {
+	return new Promise(((success, failure) => {
 		PrepareHTTP();
-		var db = new DB(settings.uri + '/' + settings.database, settings.hooks, self);
+		const db = new DB(`${settings.uri}/${settings.database}`, settings.hooks, self);
 		db.connect()
-			.then(function (d) {
+			.then((d) => {
 				self.db = d;
-				var auth = new Auth(self)
-					.then(function (s) {
+				const auth = new Auth(self)
+					.then((s) => {
 						collections = _.clone(self.db.collections);
-						while ((collection = collections.pop()) !== undefined) {
+						while ((collection = collections.pop()) !== undefined)
 							if (collection.trim() !== '' && collection != 'system.indexes') {
-								self.sockets[collection] = self.io.of('/' + settings.database + '/' + collection);
-								self.socketservers[collection] = new SocketServer(self.io, collection, self.db); //SetUpSockets( collection );
-								if (self.settings.http) {
-									self.settings.http.use('/' +
-										settings.database + '/' + collection, new reststop(collection, self.db, self.io.of('/' + settings.database + '/' + collection)));
-								}
+								self.sockets[collection] = self.io.of(`/${settings.database}/${collection}`);
+								self.socketservers[collection] = new SocketServer(self.io, collection, self.db); // SetUpSockets( collection );
+								if (self.settings.http)
+									self.settings.http.use(`/${
+										settings.database}/${collection}`, new reststop(collection, self.db, self.io.of(`/${settings.database}/${collection}`)));
 							}
-						}
 
-						if (typeof self.settings.onComplete == 'function') {
+
+						if (typeof self.settings.onComplete === 'function')
 							self.settings.onComplete(self, _, ObjectId);
-						}
+
 
 						success(self);
 					});
-			}, function (err) {
+			}, (err) => {
 				failure(err);
 				console.error('Unable to connect to database: ', err);
 			});
-	});
+	}));
 };
 
 Highway.prototype.LoadRoutes = function (routes) {
-	var self = this,
+	let self = this,
 		route;
-	var _ = require('underscore');
-	if (!_.isArray(routes)) {
+	const _ = require('underscore');
+	if (!_.isArray(routes))
 		routes = [routes];
-	}
-	for (var i in routes) {
+
+	for (const i in routes) {
 		route = routes[i];
 		console.log(route);
-		if (!route.method || ['get','post'].indexOf(route.method.toLowerCase()==-1)) {
-			console.error('Routes must have a method of either GET or POST, your route has: "' + route.method + '"');
+		if (!route.method || ['get', 'post'].indexOf(route.method.toLowerCase()) == -1) {
+			console.error(`Routes must have a method of either GET or POST, your route has: "${route.method}"`);
 		} else if (typeof route.handler() !== 'function') {
 			console.error('Route handlers passed to LoadRoute must return an function');
 		} else {
